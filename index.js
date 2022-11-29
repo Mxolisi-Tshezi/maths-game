685d2a894c2af7d96d975458b8979013db69ca1d
Mxolisi <nyawose57@gmail.com>
2022-11-29 15:04:17 +0200

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const {sum, sub, mult, div} = require("./math")
+// const {sum, sub, mult, div} = require("./math")
 
-console.log(mult(10,5)
+// console.log(mult(10,5)
+
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 1967;
+
+app.get('/', (req, res) => res.send('Maths game!'));
+
+app.listen(PORT, function () {
+    console.log('App starting on port', PORT);
+  });
--- math.js
+++ math.js
@@ -1,17 +1,27 @@
-const sum = (a,b) =>{
-  return a + b
-}
+// const sum = (a,b) =>{
+//   return a + b
+// }
 
-const sub = (a,b) =>{
-  return a - b
-}
+// const sub = (a,b) =>{
+//   return a - b
+// }
 
-const mult = (a,b) =>{
-  return a * b
-}
+// const mult = (a,b) =>{
+//   return a * b
+// }
 
-const div = (a,b) =>{
-  return a/b
-}
+// const div = (a,b) =>{
+//   return a/b
+// }
 
-module.export = {sum, sub, mult, div}
+// module.export = {sum, sub, mult, div}
+
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 1967;
+
+app.get('/', (req, res) => res.send('Maths game!'));
+
+app.listen(PORT, () => console.log(`App started : ${PORT}`))
