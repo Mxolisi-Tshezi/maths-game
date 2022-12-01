65440e7f4de0760b45336c7f46bc6fcd290f76cf
Mxolisi <nyawose57@gmail.com>
2022-12-01 16:29:02 +0200

    saving changes on index.handlebars

---
+++ public/index.js
@@ -0,0 +1,17 @@
+
+
+let one_count="";
+let two_count="";
+let oneElement=document.querySelector('.one_count');
+let twoElement=document.querySelector('.two_count');
+
+oneElement.innerText=one_count;
+twoElement.innerText=two_count;
+const operators=document.querySelector('.operators');
+
+// if (typeof window !== 'undefined') {
+//     console.log('You are on the browser')
+//   } else {
+//     console.log('You are on the server')
+//   }
+  
