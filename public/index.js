d270a24187943758f3d10188ce42a16182c161fd
Mxolisi <nyawose57@gmail.com>
2022-12-02 06:20:31 +0200

    changed model

--- public/index.js
+++ public/index.js
@@ -1,19 +1,17 @@
-
-
 let one_count="";
-let two_count="";
-let three_count="";
+let thesum="";
+
 let oneElement=document.querySelector('.one_count');
-let twoElement=document.querySelector('.two_count');
-let threeElement=document.querySelector('.three_count');
-
-
 oneElement.innerText=one_count;
-twoElement.innerText=two_count;
-threeElement.innerText=three_count;
-
 
 const operators=document.querySelector('.operators');
+var addBtn = document.querySelector(".operand");
+
+let thesumElement=document.querySelector('.thesum');
+thesumElement.innerText = thesum;
+
+
+
 
 // if (typeof window !== 'undefined') {
 //     console.log('You are on the browser')
